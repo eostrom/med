ef3643b723db0471bee04b3db61f7fd805b325e2
Erik Ostrom <erik@echographia.com>
2011-06-09 14:52:57 -0500

    Added genre option.

--- lib/med/med_command.rb
+++ lib/med/med_command.rb
@@ -7,7 +7,8 @@ module Med
     
     subcommand 'add', 'Add an audio file to the iTunes library.' do
       parameter 'FILE', 'the file to add', :attribute_name => :path
-
+      option ['-g', '--genre'], 'GENRE', "the track's genre"
+      
       def execute
         url = NSURL.fileURLWithPath(path)
         track = ITunes.app.add(url, to:nil)
@@ -15,11 +16,12 @@ module Med
           track.artist = track.albumArtist = 'Erik Ostrom'
           track.album = 'Music Every Day'
           track.year = Time.now.year
+          track.genre = genre || 'Electronic'
           if track.name =~ /^([0-9]+) (.*)/
             track.trackNumber = $1.to_i
             track.name = $2
           end
-          
+              
           puts "Added #{path} to iTunes"
         else
           puts "ERROR: Couldn't add #{path} to iTunes"
